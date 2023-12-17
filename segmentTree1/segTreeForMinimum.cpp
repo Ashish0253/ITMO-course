54ecc75e14103772d898227d98614d98978cd561
Ashish0253 <ashishrana0253@gmail.com>
2023-12-18 01:50:50 +0530

    segTreeForMinimum done

--- segmentTree1/segTreeForMinimum.cpp
+++ segmentTree1/segTreeForMinimum.cpp
@@ -4,71 +4,72 @@ using namespace std;
 struct segTree {
 
     int size;
-    vector<long long> sums;
+    vector<int> values;
 
     void init(int n) {
         size = 1;
         while(size < n) size *= 2;
-        sums.assign(2 * size, 0LL);
+        values.assign(2 * size, INT_MAX);
     }
 
     void build(vector<int> &a, int x, int lx, int rx) {
         if(rx - lx == 1) {
             if(lx < (int)a.size()) {
-                sums[x] = a[lx];
+                values[x] = a[lx];
             }
             return;
-        }
+        } 
 
         int m = (lx + rx) / 2;
         build(a, 2 * x + 1, lx, m);
         build(a, 2 * x + 2, m, rx);
-        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];    
+        values[x] = min(values[2 * x + 1], values[2 * x + 2]);
     }
 
     void build(vector<int> &a) {
         build(a, 0, 0, size);
     }
-    
-    void set(int i, int v, int x, int  lx, int rx) {
+
+    void set(int i, int v, int x, int lx, int rx) {
         if(rx - lx == 1) {
-            sums[x] = v;
+            values[x] = v;
             return;
-        }
-        
+        } 
+
         int m = (lx + rx) / 2;
         if(i < m) {
             set(i, v, 2 * x + 1, lx, m);
-        } 
+        }
         else {
             set(i, v, 2 * x + 2, m, rx);
         }
-        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
+        values[x] = min(values[2 * x + 1], values[2 * x + 2]);
     }
 
     void set(int i, int v) {
         set(i, v, 0, 0, size);
     }
 
-    long long sum(int l, int r, int x, int lx, int rx) {
-        if(l >= rx || r <= lx) return 0;
-        if(l <= lx && rx <= r) return sums[x];
+    int calculateMin(int l, int r, int x, int lx, int rx) {
+        if(r <= lx || rx <= l) return INT_MAX;
+        if(l <= lx && rx <= r) return values[x];
 
         int m = (lx + rx) / 2;
-        long long s1 = sum(l, r, 2 * x + 1, lx, m);
-        long long s2 = sum(l, r, 2 * x + 2, m, rx);
-        return s1 + s2;
+        int v1 = calculateMin(l, r, 2 * x + 1, lx, m);
+        int v2 = calculateMin(l, r, 2 * x + 2, m, rx);
+        return min(v1, v2);
     }
 
-    long long sum(int l, int r) {
-        return sum(l, r, 0, 0, size);
+    int calculateMin(int l, int r) {
+        return calculateMin(l, r, 0, 0, size);
     }
 
     void print() {
-        for(auto i: sums) 
+        for(auto i: values)
             cout << i << " ";
         cout << endl;
     }
+
 };
 
 int main() {
@@ -105,9 +106,7 @@ int main() {
             int l, r;
             cin >> l >> r;
 
-            cout << str.sum(l, r) << endl;
+            cout << str.calculateMin(l, r) << endl;
         }
     }
-
-    // str.print();
 }
